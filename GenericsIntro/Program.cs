b280c86eecb6704161920c860e0877ec1ae81bb8
Mu Tu <hadvidub@gmail.com>
2021-01-20 23:50:10 +0300

    16 Ocak Ders Sonu

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Murat");
+            isimler.Add("Turan");
+            isimler.Add("Halil");
+
+            foreach (MyList isim in isimler)
+                {
+                Console.WriteLine(isim);
+            }
+        }
+    }
+}
